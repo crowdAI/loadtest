fdb030ed7c5ab967626b76ad0c60759ac395e42b
Sean Carroll <sfcarroll@gmail.com>
2017-05-10 12:08:04 +0200

    more tests

--- loadtest.py
+++ loadtest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# cd /tmp;virtualenv env;source env/bin/activate;pip install git+https://github.com/spMohanty/crowdai-client-py;python -c 'import crowdai;challenge = crowdai.Challenge("GeccoOptimizationChallenge2017", "4f2b61e1aaf03d3283f135febbe225a4");response = challenge.evaluate_parallel([[1, 2, 3]]*5);challenge.submit([1,2,3])'
+# pip install git+https://github.com/spMohanty/crowdai-client-py
+
 import sys
 import crowdai
 
@@ -8,7 +9,7 @@ import crowdai
 #api_key = str(api_key).strip()
 
 api_keys = [
-
+keys here
 ]
 
 
@@ -16,6 +17,26 @@ def make_submission(api_key):
   challenge = crowdai.Challenge("GeccoOptimizationChallenge2017", api_key)
   print challenge
   response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+  response = challenge.evaluate_parallel([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+
   print response
   response = challenge.submit([1,2,3])
   print response
